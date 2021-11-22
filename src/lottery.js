5a553f64c8fb2fcf1a86ba138c669c04838145d6
rahilpopat369 <68607333+rahilpopat369@users.noreply.github.com>
2021-11-22 17:15:57 +0000

    Delete src directory

--- src/lottery.js
+++
@@ -1,9 +0,0 @@
-import web3 from './web3';
-
-const address = '0x29F26196d00bfB2fd26A812ff70b398b4bE3e005';
-const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
-// this line below connects to the local instance of our contract which 
-// has been deployed on the EVM
-//const contractJson = fs.readFileSync('path_to_abi_file.json');
-//const abi = JSON.parse(contractJson);
-export default new web3.eth.Contract(abi, address);
